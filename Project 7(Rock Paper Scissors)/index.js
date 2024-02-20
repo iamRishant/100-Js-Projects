32acf0c6ac4628551af0ffe71c2ffd0b43d924bb
Rishant Rana <kiturana378@gmail.com>
2024-02-20 21:56:14 +0530

    Project 7 algo correction

--- Project 7(Rock Paper Scissors)/index.js
+++ Project 7(Rock Paper Scissors)/index.js
@@ -10,6 +10,31 @@ let message=document.querySelector(".message");
 // let newval=Number(score)+1;
 // userScore.textContent=newval;
 // console.log(options[0]);
+
+
+function checkWinner(u,c){
+    if(u==c){
+        show.textContent="Draw hogya....Try again";
+        message.classList.remove("winner");
+        message.classList.remove("looser");
+        message.classList.add("draw");
+
+    }
+    else if((u==0 && c==2) || (u==1 && c==0) || (u==2 && c==1)){
+             userScore.textContent=Number(userScore.textContent)+1;
+            show.textContent="Are Wah....Kya baat hai...You won";
+            message.classList.add("winner");
+            message.classList.remove("looser");
+            message.classList.remove("draw");
+    }
+    else{
+        botScore.textContent=Number(botScore.textContent)+1;
+        show.textContent="Kya Yaar....Bot se haar rhe ho...";
+        message.classList.remove("winner");
+        message.classList.add("looser");
+        message.classList.remove("draw");
+    }
+}
 options.forEach((option,index)=>{
     option.addEventListener("click",()=>{
         // console.log(option.getAttribute("id"));
@@ -17,33 +42,7 @@ options.forEach((option,index)=>{
         var randomNumber = Math.random() * 3;
         var randomInteger = Math.floor(randomNumber);
 
-        if(index>randomInteger){
-            // console.log(index,randomInteger);
-            // console.log("player wins");
-            userScore.textContent=Number(userScore.textContent)+1;
-            show.textContent="Are Wah....Kya baat hai...You won";
-            message.classList.add("winner");
-            message.classList.remove("looser");
-            message.classList.remove("draw");
-            
-        }
-        else if(index<randomInteger){
-            // console.log(index,randomInteger);
-            // console.log("bot wins");
-            botScore.textContent=Number(botScore.textContent)+1;
-            show.textContent="Kya Yaar....Bot se haar rhe ho...";
-            message.classList.remove("winner");
-            message.classList.add("looser");
-            message.classList.remove("draw");
-        }
-        else{
-            // console.log(index,randomInteger);
-            // console.log("Draw");
-            show.textContent="Draw hogya....Try again";
-            message.classList.remove("winner");
-            message.classList.remove("looser");
-            message.classList.add("draw");
-        }
+        checkWinner(index, randomInteger);
 
     })
 })
